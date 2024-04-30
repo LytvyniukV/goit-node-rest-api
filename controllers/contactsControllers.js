1c75f49b33f02b9e254aa2b983a0e820d1693c63
LytvyniukV <lytvyniuk.vladyslav@gmail.com>
2024-04-30 18:46:12 +0200

    fixed

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -12,11 +12,10 @@ export const getAllContacts = async (req, res, next) => {
     const contacts = await listContacts();
     res.json({
       status: "success",
-      code: 200,
       data: contacts,
     });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
 
@@ -24,20 +23,14 @@ export const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await getContactById(id);
-    if (contact) {
-      res.json({
-        status: "success",
-        code: 200,
-        data: contact,
-      });
-    } else {
-      const err = HttpError(404);
-      res.status(err.status).json({
-        message: err.message,
-      });
-    }
+    if (!contact) throw HttpError(404, "Contact not found");
+
+    res.json({
+      status: "success",
+      data: contact,
+    });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
 
@@ -45,20 +38,14 @@ export const deleteContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const deletedContact = await removeContact(id);
-    if (deletedContact) {
-      res.json({
-        status: "success",
-        code: 200,
-        data: deletedContact,
-      });
-    } else {
-      const err = HttpError(404);
-      res.status(err.status).json({
-        message: err.message,
-      });
-    }
+    if (!deletedContact) throw HttpError(404, "Contact not found");
+
+    res.json({
+      status: "success",
+      data: deletedContact,
+    });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
 
@@ -68,11 +55,10 @@ export const createContact = async (req, res, next) => {
     const newContact = await addContact(name, email, phone);
     res.status(201).json({
       status: "created",
-      code: 201,
       data: newContact,
     });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
 
@@ -80,20 +66,14 @@ export const updateContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const updatedContact = await updContact(id, req.body);
-    if (updatedContact) {
-      res.status(200).json({
-        status: "success",
-        code: 200,
-        data: updatedContact,
-      });
-    } else {
-      const err = HttpError(404);
-      res.status(err.status).json({
-        message: err.message,
-      });
-    }
+    if (!updatedContact) throw HttpError(404, "Contact not found");
+
+    res.status(200).json({
+      status: "success",
+      data: updatedContact,
+    });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
 
@@ -101,19 +81,13 @@ export const updateFavoriteContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const updatedContact = await updContact(id, req.body);
-    if (updatedContact) {
-      res.status(200).json({
-        status: "success",
-        code: 200,
-        data: updatedContact,
-      });
-    } else {
-      const err = HttpError(404);
-      res.status(err.status).json({
-        message: err.message,
-      });
-    }
+    if (!updatedContact) throw HttpError(404, "Contact not found");
+
+    res.status(200).json({
+      status: "success",
+      data: updatedContact,
+    });
   } catch (error) {
-    next(HttpError(500));
+    next(HttpError(error));
   }
 };
